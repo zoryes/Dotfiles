e24f0e1de7fd41a879d8f3a58cb855287a17630e
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-26 00:32:50 +0300

    added c

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,12 @@
 require("nvim-treesitter").install({
 	"lua",
+	"c",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = {
 		"lua",
+		"c",
 	},
 	callback = function()
 		vim.treesitter.start()
