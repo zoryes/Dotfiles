0d3cfa1c4a8e367c73af6f5a467e34d636b7b63e
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-25 23:47:40 +0300

    added mappings

---
+++ nvim/lua/config/mappings.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
