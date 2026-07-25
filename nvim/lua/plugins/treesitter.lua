48277fc666f7366126993fa9f830ae885405578c
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-26 01:54:50 +0300

    added swift

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 require("nvim-treesitter").install({
 	"lua",
 	"c",
+	"swift",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = {
 		"lua",
 		"c",
+		"swift",
 	},
 	callback = function()
 		vim.treesitter.start()
