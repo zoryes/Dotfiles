0878d0c125923483146ba2eadbb95261d977c034
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-26 12:59:37 +0300

    search esc

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,7 +1,13 @@
+-- buffer movement
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- clear search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", {
+	desc = "Clear search highlight",
+})
 
 -- autoclose quickfix window
 vim.api.nvim_create_autocmd("FileType", {
