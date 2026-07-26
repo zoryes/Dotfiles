8e7eb09146b8051c9b6f15f6b67267516158075b
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-26 13:04:15 +0300

    tree config

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -10,3 +10,11 @@ require("nvim-tree").setup({
 })
 
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeToggle)
+
+-- open tree on start and move to text buffer
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		require("nvim-tree.api").tree.open()
+		vim.cmd("wincmd p")
+	end,
+})
