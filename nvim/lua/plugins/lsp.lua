369ffaf83e9ae7a761389c9325d141f02a348998
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-07-26 12:28:52 +0300

    lsp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,4 +6,30 @@ require("mason-lspconfig").setup({
 	},
 })
 
-vim.lsp.enable("lua_ls")
+local map = vim.keymap
+local diag = vim.diagnostic
+local lsp = vim.lsp
+
+-- global lsp mappings
+local on_attach = function(client, bufnr)
+	local opts = { noremap = true, silent = true }
+	-- open diagnostic
+	map.set("n", "<leader>vd", function()
+		diag.open_float()
+	end, opts)
+	-- next diagnostic
+	map.set("n", "[d", function()
+		diag.goto_next()
+	end, opts)
+	-- prev diagnostic
+	map.set("n", "]d", function()
+		diag.goto_prev()
+	end, opts)
+end
+
+-- apply the globals to all languages
+lsp.config("lua_ls", {
+	on_attach = on_attach,
+})
+
+lsp.enable("lua_ls")
