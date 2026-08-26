c641bc608cd46694efc7f1527b79a4484e26a230
Alexandru Cuta <alex@Alexandrus-MacBook-Air.local>
2026-08-26 16:51:23 +0300

    gitsigns update

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -17,18 +17,18 @@ require("gitsigns").setup({
 	},
 	signs_staged_enable = true,
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		follow_files = true,
 	},
 	auto_attach = true,
 	attach_to_untracked = false,
-	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
 		delay = 1000,
 		ignore_whitespace = false,
 		virt_text_priority = 100,
